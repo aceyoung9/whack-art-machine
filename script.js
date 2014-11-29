b37df47e012724c7d87357a07b5c44d3b63cb746
Alice Young <aliceyoung.9@gmail.com>
2014-11-29 02:35:05 -0500

    I've got a triangle function!

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
+function triangle(canvas,len) {
+  // Makes an equilateral traingle, given length of side, and surface to draw
+  // on.
+  canvas.beginPath();
+  canvas.moveTo(0, len * (Math.sqrt(3)/2));
+  canvas.lineTo(len, len * (Math.sqrt(3)/2));
+  canvas.lineTo(len/2, 0);
+  canvas.fill();
+}
+
 function draw() {
   var canvas = document.getElementById('myCanvas');
   if (canvas.getContext){
     var ctx = canvas.getContext('2d');
-
-    ctx.beginPath();
-    ctx.moveTo(75,50);
-    ctx.lineTo(100,75);
-    ctx.lineTo(100,25);
-    ctx.fill();
+    triangle(ctx, 300);
   }
 }
+
 draw();
