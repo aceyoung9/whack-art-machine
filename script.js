4f08eb36dbf9d2ef2e4bb84133dde6f21db4330e
Alice Young <aliceyoung.9@gmail.com>
2014-12-02 21:49:42 -0500

    randomized blues and centered triangles.

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
+// Returns a random integer between min (included) and max (excluded)
+// Using Math.round() will give you a non-uniform distribution!
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+var canvas = document.getElementById('myCanvas');
+var width = myCanvas.width;
+var height = myCanvas.height;
+
 function triangle(canvas,len,x,y) {
   canvas.beginPath();
-  canvas.moveTo(x, (len * (Math.sqrt(3)/2)) + y);
-  canvas.lineTo(len + x, (len * (Math.sqrt(3)/2) + y));
-  canvas.lineTo(x + len/2, y);
+  canvas.moveTo(x,y);
+  canvas.lineTo(x + len, y);
+  canvas.lineTo(x + len/2, y - (len * (Math.sqrt(3)/2)));
+
+  canvas.fillStyle = "rgb("+ getRandomInt(0, 200) 
+    + ", " + getRandomInt(190, 220)
+    + "," + getRandomInt(190, 220) + ")";
   canvas.fill();
 }
 
 function draw() {
-  var canvas = document.getElementById('myCanvas');
   if (canvas.getContext){
     var ctx = canvas.getContext('2d');
     //triangle(ctx, 300);
-    triangle(ctx, 200, 0, 0);
-    triangle(ctx, 200, 100, 100);
+    //triangle(ctx, 200, 0, 200);
+    triangle(ctx, 100, width/2, height/2);
   }
 }
 
